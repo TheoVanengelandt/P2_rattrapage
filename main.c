10cb299f7548e5252f614132ae38672d7e86e644
TheoVanengelandt <theo.vanengelandt@viacesi.fr>
2017-01-09 16:24:55 +0100

    Le main (1er jet)

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int maint (int argc, char *argv[])
+{
+	int TypeDeFond;
+	TypeDeFond=0; //rand()%3; //renvoi une valeur pseudo-aléatoire (0, 1 ou 2), correspondant au type d'ecrande veille a exec.
+
+	switch(TypeDeFond)
+	{
+		case 0:
+			system("./static");
+			break;
+		case 1:
+			//exec program static
+			break;
+		case 2:
+			//exec program static
+			break;
+	}
+
+	quit();
+	printf("Le processus ne c'est pas arrêté"); //vérifie l'arrêt du processus
+	return 0;
+}
