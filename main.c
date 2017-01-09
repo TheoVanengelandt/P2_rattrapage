a7a62aa701923b2a5c0f4d061d3e6f8f9420bbe2
TheoVanengelandt <theo.vanengelandt@viacesi.fr>
2017-01-09 16:43:17 +0100

    maint 1.1

--- main.c
+++ main.c
@@ -3,22 +3,28 @@
 
 int maint (int argc, char *argv[])
 {
-	int TypeDeFond;
-	TypeDeFond=0; //rand()%3; //renvoi une valeur pseudo-aléatoire (0, 1 ou 2), correspondant au type d'ecrande veille a exec.
-
-	switch(TypeDeFond)
+	if(argv[0]=="stat")
 	{
-		case 0:
-			system("./static");
-			break;
-		case 1:
-			//exec program static
-			break;
-		case 2:
-			//exec program static
-			break;
+		system("./log");
 	}
+	else
+	{
+		int TypeDeFond;
+		TypeDeFond=0; //rand()%3; //renvoi une valeur pseudo-aléatoire (0, 1 ou 2), correspondant au type d'ecrande veille a exec.
 
+		switch(TypeDeFond)
+		{
+			case 0:
+				system("./static");
+				break;
+			case 1:
+				//exec program static
+				break;
+			case 2:
+				//exec program static
+				break;
+		}
+	}	
 	quit();
 	printf("Le processus ne c'est pas arrêté"); //vérifie l'arrêt du processus
 	return 0;
