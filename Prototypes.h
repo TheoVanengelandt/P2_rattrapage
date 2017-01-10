f9c89ebd687ce72fafd61d99b5967a4c712ce3cc
TheoVanengelandt <theo.vanengelandt@viacesi.fr>
2017-01-10 09:15:38 +0100

    Prototypes
    ajout des prototypes

---
+++ Prototypes.h
@@ -0,0 +1,14 @@
+#ifndef PROTOTYPES_H_INCLUDED
+#define PROTOTYPES_H_INCLUDED
+
+int quit();
+int testASCII();
+
+
+int EcranStatic(int** Ecran);
+
+void Type1(int i);
+void Type2(int i,int j,int l);
+
+
+#endif // PROTOTYPES_H_INCLUDED
